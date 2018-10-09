ee8a89ce4b350e1606acb2a917bcad04eec32c36
Daksh Pareek <dakshpareek7@gmail.com>
2018-10-09 07:59:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def login():
 	link="https://www.amazon.com/ap/signin?accountStatusPolicy=P1&clientContext=262-8850897-1873444&language=en_US&openid.assoc_handle=amzn_prime_video_desktop_us&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.primevideo.com%2Fauth%2Freturn%2Fref%3Dav_auth_ap%3F_encoding%3DUTF8%26location%3D%252Fref%253Ddv_auth_ret"
 	amazonDriver.get(link)
 	time.sleep(2)
-	amazonDriver.find_element_by_name('email').send_keys("+917597186191")
-	amazonDriver.find_element_by_name('password').send_keys("cockroach")
+	amazonDriver.find_element_by_name('email').send_keys("")
+	amazonDriver.find_element_by_name('password').send_keys("")
 	time.sleep(1)
 	amazonDriver.find_element_by_xpath('//*[@id="signInSubmit"]').click()
 	time.sleep(1)
